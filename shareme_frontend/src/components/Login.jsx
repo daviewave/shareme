b75bb3bb982d556fe476bc1cc2836cbc06e69d91
David Silveira <daviewave@gmail.com>
2023-05-04 20:27:29 -0400

    added a black overlay to entire screen

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -17,7 +17,12 @@ const Login = () => {
           muted={true}          
           autoPlay
           className='w-full h-full object-cover'
-        />        
+        />
+
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+
+        </div>
+
       </div>
     </div>
   )
