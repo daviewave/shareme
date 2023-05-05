186f71862689cb709411881e99a42f618af96f37
David Silveira <daviewave@gmail.com>
2023-05-05 12:23:58 -0400

    wrapped the router DOM in the google oauth login package

--- shareme_frontend/src/index.js
+++ shareme_frontend/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom/'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom/';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-import App from './App'
-import './index.css'
+import App from './App';
+import './index.css';
 
 // hooks the application onto the DOM
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_CLIENT_ID}>
+    <Router>
+      <App />
+    </Router>
+  </GoogleOAuthProvider>,
   document.getElementById('root')
-)
+);
