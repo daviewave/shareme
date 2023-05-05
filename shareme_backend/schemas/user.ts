99fdfcefee780bf41e7685b34cf3b9e191afb309
David Silveira <daviewave@gmail.com>
2023-05-05 12:27:56 -0400

    fixed issue in user schema

--- shareme_backend/schemas/user.ts
+++ shareme_backend/schemas/user.ts
@@ -1,17 +1,17 @@
 export default {
-    name: 'user',
-    title: 'User',
-    type: 'document',
-    fields: [
-        {
-            name: 'name',
-            title: 'UserName',
-            type: 'string',
-        },
-        {
-            name: 'image',
-            title: 'Image',
-            type: 'string',
-        },
-    ],
-}
+  name: 'user',
+  title: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'username',
+      title: 'UserName',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'string',
+    },
+  ],
+};
