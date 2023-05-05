7b2b67c5bf1df2d6ecbe501d054cb12f038ae1c7
David Silveira <daviewave@gmail.com>
2023-05-05 12:23:26 -0400

    added google oauth login code and redirect the user to the Home page upon successully logging in

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,31 +1,102 @@
-import React from 'react';
-import GoogleLogin from 'react-google-login';
+import React, { useState, useEffect } from 'react';
+import { googleLogout, useGoogleLogin, GoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
+import { client } from '../client';
+
 import { useNavigate } from 'react-router-dom';
-import FcGoogle from 'react-icons/fc';
 import shareVid from '../assets/share.mp4';
-import logo from '../assets/logo.png';
+import logo from '../assets/logowhite.png';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState([]);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => {
+      setUser(codeResponse);
+    },
+    onError: (error) => console.log('Login Failed:', error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: 'application/json',
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+
+          const newUserDocument = {
+            _id: res.data.id,
+            _type: 'user',
+            userName: res.data.name,
+            image: res.data.picture,
+          };
+
+          client.createIfNotExists(newUserDocument).then(() => {
+            navigate('/', { replace: true });
+          });
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
+
+  console.log('user', user);
+  console.log('profile', profile);
+
   return (
     <div className="flex justify-start items-center flex-col h-screen">
-      <div className='relative w-full h-full'>
+      <div className="relative w-full h-full">
         <video
           src={shareVid}
-          type='video/mp4'
+          type="video/mp4"
           loop
           controls={false}
-          muted={true}          
+          muted={true}
           autoPlay
-          className='w-full h-full object-cover'
+          className="w-full h-full object-cover"
         />
 
-        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
 
+          <div className="shadow-2xl">
+            {profile ? (
+              <div>
+                <img src={profile.picture} alt="user image" />
+                <h3>User Logged in</h3>
+                <p>Name: {profile.name}</p>
+                <p>Email Address: {profile.email}</p>
+                <br />
+                <br />
+                <button onClick={logOut}>Log out</button>
+              </div>
+            ) : (
+              <GoogleLogin onSuccess={() => login()} onError={() => login()}>
+                <button onClick={() => login()}>Sign in with Google 🚀 </button>
+              </GoogleLogin>
+            )}
+          </div>
         </div>
-
       </div>
     </div>
-  )
+  );
 };
 
 export default Login;
