d9d9e5d526b1c6ba50d34db9f87614c78d49c07c
David Silveira <daviewave@gmail.com>
2023-05-15 13:43:01 -0400

    added search and feed queries

--- shareme_frontend/src/utils/data.js
+++ shareme_frontend/src/utils/data.js
@@ -1,3 +1,51 @@
 export const userQuery = (userId) => {
   return `*[_type == "user" && _id == '${userId}']`;
 };
+
+export const searchQuery = (searchTerm) => {
+  return `*[_type == "pin" && title match "${searchTerm}*" || category match "${searchTerm}*" || about match "${searchTerm}*"]{
+      image {
+        asset -> {
+          url
+        }
+      },
+      _id,
+      destination,
+      postBy -> {
+        _id,
+        username,
+        image
+      },
+      save[] {
+        _key,
+        postedBy -> {
+          _id,
+          username,
+          image
+        },
+      },
+  }`;
+};
+
+export const feedQuery = `*[_type == "pin"] | order(_createdAt desc){
+  image {
+    asset -> {
+      url
+    }
+  },
+  _id,
+  destination,
+  postBy -> {
+    _id,
+    username,
+    image
+  },
+  save[] {
+    _key,
+    postedBy -> {
+      _id,
+      username,
+      image
+    },
+  },
+}`;
