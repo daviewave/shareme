8778a71aa07ae1b8f68321070b90365e58a2f21c
David Silveira <daviewave@gmail.com>
2023-05-15 18:59:30 -0400

    added all relevant queries

--- shareme_frontend/src/utils/data.js
+++ shareme_frontend/src/utils/data.js
@@ -66,54 +66,169 @@ export const categories = [
   },
 ];
 
-export const userQuery = (userId) => {
-  return `*[_type == "user" && _id == '${userId}']`;
-};
-
-export const searchQuery = (searchTerm) => {
-  return `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
-      image {
-        asset -> {
-          url
-        }
-      },
-          _id,
-          destination,
-          postedBy -> {
-            _id,
-            username,
-            image
-          },
-          save[] {
-            _key,
-            postedBy -> {
-              _id,
-              username,
-              image
-            },
-          },
-  }`;
-};
-
-export const feedQuery = `*[_type == "pin"] | order(_createdAt desc){
-  image {
-    asset -> {
+export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+  image{
+    asset->{
       url
     }
   },
-  _id,
-  destination,
-  postedBy -> {
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        username,
+        image
+      },
+      save[]{
+        _key,
+        postedBy->{
+          _id,
+          username,
+          image
+        },
+      },
+    } `;
+
+export const pinDetailQuery = (pinId) => {
+  const query = `*[_type == "pin" && _id == '${pinId}']{
+    image{
+      asset->{
+        url
+      }
+    },
     _id,
-    username,
-    image
-  },
-  save[] {
-    _key,
-    postedBy -> {
+    title, 
+    about,
+    category,
+    destination,
+    postedBy->{
       _id,
       username,
       image
     },
-  },
-}`;
+   save[]{
+      postedBy->{
+        _id,
+        username,
+        image
+      },
+    },
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+        _id,
+        username,
+        image
+      },
+    }
+  }`;
+  return query;
+};
+
+export const pinDetailMorePinQuery = (pin) => {
+  const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      username,
+      image
+    },
+    save[]{
+      _key,
+      postedBy->{
+        _id,
+        username,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
+export const searchQuery = (searchTerm) => {
+  const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+        image{
+          asset->{
+            url
+          }
+        },
+            _id,
+            destination,
+            postedBy->{
+              _id,
+              username,
+              image
+            },
+            save[]{
+              _key,
+              postedBy->{
+                _id,
+                username,
+                image
+              },
+            },
+          }`;
+  return query;
+};
+
+export const userQuery = (userId) => {
+  const query = `*[_type == "user" && _id == '${userId}']`;
+  return query;
+};
+
+export const userCreatedPinsQuery = (userId) => {
+  const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      username,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        username,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
+export const userSavedPinsQuery = (userId) => {
+  const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      username,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        username,
+        image
+      },
+    },
+  }`;
+  return query;
+};
