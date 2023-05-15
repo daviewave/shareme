83418c08dd14d7fb6cac452ccdd87df127afa7c5
David Silveira <daviewave@gmail.com>
2023-05-15 16:18:56 -0400

    created a global fetchUser function

--- shareme_frontend/src/pages/Home.jsx
+++ shareme_frontend/src/pages/Home.jsx
@@ -9,6 +9,7 @@ import { client } from '../client';
 import logo from '../assets/logo.png';
 
 import { userQuery } from '../utils/data';
+import { fetchUser } from '../utils/fetchUser';
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
@@ -16,10 +17,7 @@ const Home = () => {
 
   const scrollRef = useRef(null);
 
-  const userInfo =
-    localStorage.getItem('user') !== undefined
-      ? JSON.parse(localStorage.getItem('user'))
-      : localStorage.clear();
+  const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.id);
