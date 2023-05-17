bc5f168f709a3c62cbb51ddfea4878b16c37f4dc
David Silveira <daviewave@gmail.com>
2023-05-17 12:29:06 -0400

    added pin detail component

--- shareme_frontend/src/components/PinDetail.jsx
+++ shareme_frontend/src/components/PinDetail.jsx
@@ -16,6 +16,54 @@ const PinDetail = ({ user }) => {
 
   const { pinId } = useParams();
 
+  const addComment = () => {
+    if (comment) {
+      setAddingComment(true);
+
+      const newComment = {
+        comment,
+        _key: uuidv4(),
+        postedBy: {
+          _type: 'postedBy',
+          _ref: user._id,
+        },
+      };
+
+      // Optimistically add the new comment to local state
+      setPinDetail((prevPinDetail) => ({
+        ...prevPinDetail,
+        comments: [...prevPinDetail.comments, newComment],
+      }));
+
+      client
+        .patch(pinId)
+        .setIfMissing({ comments: [] })
+        .insert('after', 'comments[-1]', [newComment])
+        .commit()
+        .then(() => {
+          setAddingComment(false);
+          setComment('');
+        })
+        .catch((error) => {
+          console.error(error);
+
+          // If the server operation fails, remove the comment from local state
+          setPinDetail((prevPinDetail) => {
+            const commentIndex = prevPinDetail.comments.findIndex(
+              (c) => c._key === newComment._key
+            );
+            return {
+              ...prevPinDetail,
+              comments: [
+                ...prevPinDetail.comments.slice(0, commentIndex),
+                ...prevPinDetail.comments.slice(commentIndex + 1),
+              ],
+            };
+          });
+        });
+    }
+  };
+
   const fetchPinDetails = () => {
     let query = pinDetailQuery(pinId);
 
@@ -40,15 +88,131 @@ const PinDetail = ({ user }) => {
 
   if (!pinDetail) return <Spinner message="Loading pin." />;
 
+  // console.log('pins: ', pinDetail.categor);
+
   return (
-    <div
-      className="flex xl:flex-row flex-col m-auto bg-white"
-      style={{ maxWidth: '1500px', borderRadius: '32px' }}
-    >
-      <div className="flex justify-center items-center md:items-start flex-initial">
-        <img src={pinDetail?.image && urlFor(pinDetail.image).url()} />
+    <>
+      <div
+        className="flex xl:flex-row flex-col m-auto bg-white"
+        style={{ maxWidth: '1500px', borderRadius: '32px' }}
+      >
+        <div className="flex justify-center items-center md:items-start flex-initial">
+          <img
+            src={pinDetail?.image && urlFor(pinDetail.image).url()}
+            alt="user-post"
+            className="rounded-t-3xl rounded-b-lg"
+          />
+        </div>
+
+        <div className="w-full p-5 flex-1 xl:min-w-620">
+          <div className="flex items-center justify-between">
+            <div className="flex gap-2 items-center">
+              <a
+                href={`${pinDetail.image?.asset?.url}?dl=`}
+                download
+                onClick={(e) => e.stopPropagation()}
+                className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+              >
+                <MdDownloadForOffline />
+              </a>
+            </div>
+
+            {/* Should go back and make this a regex to get from www --> .com and just display that */}
+            <a href={pinDetail.destination} target="_blank" rel="noreferrer">
+              {pinDetail.destination.length > 25
+                ? pinDetail.destination.slice(8, 25)
+                : pinDetail.destination}
+            </a>
+          </div>
+
+          <div>
+            <h1 className="text-4xl font-bold break-words mt-3">
+              {pinDetail.title}
+            </h1>
+            <p className="mt-3">{pinDetail.about}</p>
+          </div>
+
+          <Link
+            to={`user-profile/${pinDetail.postedBy?._id}`}
+            className="flex gap-2 mt-5 items-center bg-white rounded-lg"
+          >
+            <img
+              className="w-8 h-8 rounded-full object-cover"
+              src={pinDetail.postedBy?.image}
+              alt="user-profile"
+            />
+            <p className="font-semibold capitalize">
+              {pinDetail.postedBy?.username}
+            </p>
+          </Link>
+          <h2 className="mt-5 text-2xl">Comments</h2>
+          <div className="max-h-370 overflow-y-auto">
+            {pinDetail?.comments?.map((comment, i) => (
+              <div
+                className="flex gap-2 mt-5 items-center bg-white rounded-lg"
+                key={i}
+              >
+                <img
+                  src={comment.postedBy.image}
+                  alt="user-profile"
+                  className="w-10 h-10 rounded-full cursor-pointer"
+                />
+
+                <div className="flex flex-col">
+                  <p className="font-bold">{comment.postedBy.username}</p>
+                  <p>{comment.comment}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="flex flex-wrap mt-6 gap-3">
+            <Link to={`user-profile/${pinDetail.postedBy?._id}`}>
+              <img
+                className="w-10 h-10 rounded-full cursor-pointer"
+                src={pinDetail.postedBy?.image}
+                alt="user-profile"
+              />
+            </Link>
+
+            <input
+              className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
+              type="text"
+              placeholder="Add a comment..."
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+
+            <button
+              type="button"
+              className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
+              onClick={addComment}
+            >
+              {addingComment ? 'Posting the comment...' : 'Post'}
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+
+      {!pins ? (
+        <>
+          <Spinner message="loading more pins" />
+        </>
+      ) : pins?.length > 0 ? (
+        <>
+          <h2 className="text-center font-bold text-2xl mt-8 mb-4">
+            More like this....
+          </h2>
+          <MasonryLayout pins={pins} />
+        </>
+      ) : (
+        <>
+          <h2 className="text-center font-bold text-2xl mt-8 mb-4">
+            No similar pins found
+          </h2>
+        </>
+      )}
+    </>
   );
 };
 
