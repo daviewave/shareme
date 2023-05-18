f270b22c96662007f20faf12c9867199a060c104
David Silveira <daviewave@gmail.com>
2023-05-18 19:20:49 -0400

    added commented out code that would force the user to sign in / create an account before seeing any pins

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import Login from './components/Login';
 import Home from './pages/Home';
+// import { fetchUser } from './utils/fetchUser';
 
 const App = () => {
+  //** Uncomment this to force user to login before being able to see any pins **//
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   const user = fetchUser();
+  //   if (!user) navigate('/login');
+  // }, []);
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
